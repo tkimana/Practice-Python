185257e87e5e9ef16060a8eed98bddd031ef7154
tkimana <kimana.theodore@gmail.com>
2021-01-22 11:12:00 -0600

    done

---
+++ variables.py
@@ -0,0 +1,13 @@
+#Variable 
+
+#x = 1 #int
+#y= 2.3 #float
+#name= 'string' #string
+#is_cool= True
+
+#multiple assignment 
+x,y,name, is_cool, name=(1 , 2.4, ' string', True, ' Theo')
+
+print(x ,y ,name, is_cool, name)
+
+print(type(is_cool))
