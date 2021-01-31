bf064dda52048a58ad76bbbb9f799a1dc3a7a2cb
tkimana <kimana.theodore@gmail.com>
2021-01-31 12:39:17 -0600

    changes

---
+++ conditionals.py
@@ -0,0 +1,19 @@
+# If/Else conditions are used to decide to do something based on something being true or false
+
+
+x=10
+y=50
+
+# Comparison Operators (==, !=, >, <, >= , <=) - Used to compare values
+
+
+# If/else statement
+
+if x > y:
+    print(f'{x} is greater than {y}')
+
+else: 
+    print(f'{y} is greater than {x}')
+
+
+
