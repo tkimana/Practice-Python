5c2c18160b1bf64eb7e4d34a744feadfdb819aed
tkimana <kimana.theodore@gmail.com>
2021-02-02 13:03:43 -0600

    changes

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,7 @@
 # If/Else conditions are used to decide to do something based on something being true or false
 
 
-x=10
+x=3
 y=10
 
 # Comparison Operators (==, !=, >, <, >= , <=) - Used to compare values
@@ -9,30 +9,36 @@ y=10
 
 # If/else statement
 
-if x > y:
-    print(f'{x} is greater than {y}')
+#if x > y:
+    #print(f'{x} is greater than {y}')
 
-else: 
-    print(f'{y} is greater than {x}')
+#else: 
+    #print(f'{y} is greater than {x}')
 
 
 # elif
 
-if x > y:
-    print(f'{x} is greater than {y}')
+#if x > y:
+    #print(f'{x} is greater than {y}')
 
-elif x == y: 
-    print(f'{x} is equal to {y}')
+#elif x == y: 
+    #print(f'{x} is equal to {y}')
 
-else: 
-    print(f'{y} is greater than {x}')
+#else: 
+    #print(f'{y} is greater than {x}')
 
 
 # Nested if statement 
 
-if x> 2:
-    if x <=2:
-        print(f'{x} is greater than 2 and less than or equal to 10')
+#if x> 2:
+    #if x <=2:
+        #print(f'{x} is greater than 2 and less than or equal to 10')
 
 
+ # Membership Operators (not, not in) - Membership operators are used to test if sequence is presented in an object 
 
+
+numbers = [1,2,4,5,6]
+
+if x in numbers:
+    print(x in numbers)
