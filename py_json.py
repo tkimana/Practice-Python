da1dbeaf76ab8c5d9b41cc2b0f031aa815408842
tkimana <kimana.theodore@gmail.com>
2021-02-05 17:12:58 -0600

    changees

---
+++ py_json.py
@@ -0,0 +1,12 @@
+# JSON is commonly used with APIS. Here how we can parse JSON into a python dictionary
+
+import json
+
+userJSON='{"first_name": "TK", "last_name":, "Joe", "age": 24}'
+
+
+# Parse to dict
+
+user= json.loads(userJSON)
+
+print(user)
